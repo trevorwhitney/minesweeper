d7712d5021cbfdc26f818898c39f213d2f3ecfaa
Trevor Whitney <trevorjwhitney@gmail.com>
2020-11-07 06:44:53 -0700

    remove annoying test

--- src/test/java/test/BoardTest.java
+++ src/test/java/test/BoardTest.java
@@ -8,13 +8,6 @@ import org.junit.jupiter.api.Test;
 
 public class BoardTest {
   @Test
-  @DisplayName("prints a board based on the rows and columns it was initialized with")
-  public void printBoard() {
-    Board board = Board.builder().rows(1).columns(2).mines(0).build();
-    assertThat(board.printBoard()).isEqualTo("\t 0 1\n" + "1\t x x \n");
-  }
-
-  @Test
   @DisplayName("makeMove returns 1 if there is no mine in the spot")
   public void makeMove_1() {
     Board board = Board.builder().rows(1).columns(2).mines(0).build();
